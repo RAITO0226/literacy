1adf9eb468af8e89e7097183271faeafdf3f64dc
RAITO0226 <s26G3131DW@chibatech.ac.jp>
2026-06-24 15:29:22 +0900

    Add files via upload

---
+++ kadai2main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="kadai2.css" />
+<title>じゃんけんポン</title>
+</head>
+<body>
+<h1>第9週 Webサイト構築（2）</h1>
+<h3>課題２</h3>
+
+<div>じゃんけん</div>
+<input type="button" value="グー"   id="red_btn">
+<input type="button" value="チョキ" id="green_btn">
+<input type="button" value="パー"   id="blue_btn">
+</body>
+</html>
